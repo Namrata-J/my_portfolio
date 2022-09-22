59132dd445a0d2b453c6da87dabbab490b943ceb
riancarlosdev <riancontatoprofissional@gmail.com>
2022-09-22 03:05:44 -0300

    change files

--- src/screens/index.js
+++
@@ -1,4 +0,0 @@
-export { HomePage } from "./Home/Home";
-export { ProjectsPage } from "./Projects/Projects";
-export { BlogsPage } from "./Blogs/Blogs";
-export { PaintingsPage } from "./Paintings/Paintings";
