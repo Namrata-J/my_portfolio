1fc34cd6352ce5681835f6ba649181bf1e47143c
riancarlosdev <riancontatoprofissional@gmail.com>
2022-09-22 03:52:04 -0300

    update header

---
+++ src/components/header/index.jsx
@@ -0,0 +1,74 @@
+import { GiHamburgerMenu, GrClose } from "../../utils/icons";
+import { headerList } from "../../constants/headerLinks";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import { useState } from "react";
+import "./header.css";
+import {
+  ContentLeft,
+  Header,
+  LogoImage,
+  Title,
+  NavList,
+  ItemLink,
+} from "./styles";
+import { navList } from "./data/nav-list";
+import { useNavigate } from "react-router-dom";
+
+export const HeaderComponent = () => {
+  const getTheLinkStyle = ({ isActive }) => {
+    return isActive
+      ? {
+          color: "var(--secondary-color)",
+        }
+      : {
+          color: "var(--gray-shade)",
+        };
+  };
+
+  const [hamburgerPopUpDisplay, setHamburgerPopUpDisplay] = useState("none");
+
+  const navigate = useNavigate();
+
+  return (
+    <Header className="mp_header mp-vertically-fc">
+      <ContentLeft>
+        <LogoImage img={logo} alt="logo" />
+        <Title>Namrata Jain</Title>
+      </ContentLeft>
+      <NavList>
+        {navList.map((item, index) => (
+          <ItemLink onClick={() => navigate(item.pageLink)} key={index}>
+            {item.pageName}
+          </ItemLink>
+        ))}
+      </NavList>
+
+      <GiHamburgerMenu
+        className="mp_hamburger-icon"
+        onClick={() => setHamburgerPopUpDisplay("flex")}
+      />
+      <div
+        className="mp_hamburger-popUp-container mp-vertically-fc"
+        style={{ display: hamburgerPopUpDisplay }}
+      >
+        <div
+          className="mp_hamburger-popUp-close-icon-container"
+          onClick={() => setHamburgerPopUpDisplay("none")}
+        >
+          <GrClose className="mp_hamburger-popUp-close-icon" />
+        </div>
+        {headerList.map((item, index) => (
+          <NavLink
+            key={index}
+            className="mp_header-link mp_font1"
+            to={item.pageLink}
+            style={getTheLinkStyle}
+          >
+            {item.pageName}
+          </NavLink>
+        ))}
+      </div>
+    </Header>
+  );
+};
---
+++ src/components/header/styles.jsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  height: 100px;
+  justify-content: space-between;
+  padding: 0rem 2rem;
+  z-index: 1;
+  background: #283847;
+  position: fixed;
+  box-shadow: 0 -1px 8px #1c2a38;
+`;
+
+export const ContentLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const LogoImage = styled.div`
+  width: 80px;
+  height: 80px;
+  background-image: url(${({ img }) => img});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+export const Title = styled.h1`
+  font-family: "Aboreto", cursive;
+  font-size: 22px;
+  letter-spacing: 2px;
+  color: #fff;
+  margin-left: 20px;
+`;
+
+export const NavList = styled.nav`
+  max-width: 500px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ItemLink = styled.a`
+  font-family: "Aboreto", cursive;
+  font-weight: 600;
+  font-size: 18px;
+  color: #fff;
+  cursor: pointer;
+
+  :hover {
+    color: #dbdbdb;
+  }
+`;
