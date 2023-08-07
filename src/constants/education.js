6d49af3849fa0bf9cc499d34a2f4ecc498e425ed
Namrata <namratajain29001@gmail.com>
2023-08-08 01:31:33 +0530

    feat: updations based on binks experience

--- src/constants/education.js
+++ src/constants/education.js
@@ -1,18 +1,33 @@
 export const educationList = [
-    {
-        year: "2022",
-        description: "Part of NeoG22' web dev bootcamp where i got a chance to have hands-on experience by creating macro projects"
-    },
-    {
-        year: "2022",
-        description: "Completed Bachelor's in Computer Applications from Anand Engineering College, Agra"
-    },
-    {
-        year: "2019",
-        description: "Completed HSC with PCM+Computer from Christ The King Inter College, Tundla"
-    },
-    {
-        year: "2017",
-        description: "Completed SSC from Christ The King Inter College, Tundla"
-    }
+  {
+    year: "Dec, 2022 - Present",
+    description: "Converted to full-time engineer at Binks",
+  },
+  {
+    year: "Sep, 2022",
+    description: "Part of Roc822' cohort where i grabbed few useful skills",
+  },
+  {
+    year: "Aug, 2022",
+    description:
+      "Completed Bachelor's in Computer Applications from Anand Engineering College, Agra",
+  },
+  {
+    year: "Jun, 2022",
+    description: "Started my internship at Binks for frontend engineer role",
+  },
+  {
+    year: "Jun, 2022",
+    description:
+      "Part of NeoG22' web dev bootcamp where i got a chance to have hands-on experience by creating macro projects",
+  },
+  {
+    year: "2019",
+    description:
+      "Completed HSC with PCM+Computer from Christ The King Inter College, Tundla",
+  },
+  {
+    year: "2017",
+    description: "Completed SSC from Christ The King Inter College, Tundla",
+  },
 ];
--- src/constants/technologies.js
+++ src/constants/technologies.js
@@ -1,65 +1,100 @@
 import {
-    GrReactjs,
-    BsGithub,
-    GrHtml5,
-    TbBrandCss3,
-    SiRedux,
-    SiTypescript,
-    SiJest,
-    SiMaterialui,
-    SiNetlify,
-    SiJavascript,
-    SiReactrouter,
-    AiFillApi
+  GrReactjs,
+  BsGithub,
+  GrHtml5,
+  TbBrandCss3,
+  SiRedux,
+  SiTypescript,
+  SiJest,
+  DiMaterializecss,
+  SiNetlify,
+  SiJavascript,
+  SiReactrouter,
+  AiFillApi,
+  TbBrandNextjs,
+  SiStrapi,
+  TbBrandReactNative,
+  SiTailwindcss,
+  SiAndroidstudio,
+  SiXcode,
+  SiJira,
 } from "../utils/icons";
 
 export const technologiesList = [
-    {
-        tech: "ReactJs",
-        techLogo: <GrReactjs />
-    },
-    {
-        tech: "Github",
-        techLogo: <BsGithub />
-    },
-    {
-        tech: "HTML5",
-        techLogo: <GrHtml5 />
-    },
-    {
-        tech: "CSS3",
-        techLogo: <TbBrandCss3 />
-    },
-    {
-        tech: "Redux Toolkit",
-        techLogo: <SiRedux />
-    },
-    {
-        tech: "Typescript",
-        techLogo: <SiTypescript />
-    },
-    {
-        tech: "Jest",
-        techLogo: <SiJest />
-    },
-    {
-        tech: "Material UI",
-        techLogo: <SiMaterialui />
-    },
-    {
-        tech: "Web Hosting",
-        techLogo: <SiNetlify />
-    },
-    {
-        tech: "Javascript",
-        techLogo: <SiJavascript />
-    },
-    {
-        tech: "React Router",
-        techLogo: <SiReactrouter />
-    },
-    {
-        tech: "Context API",
-        techLogo: <AiFillApi />
-    }
+  {
+    tech: "ReactJs",
+    techLogo: <GrReactjs />,
+  },
+  {
+    tech: "ReactNative",
+    techLogo: <TbBrandReactNative />,
+  },
+  {
+    tech: "Redux Toolkit",
+    techLogo: <SiRedux />,
+  },
+  {
+    tech: "Strapi",
+    techLogo: <SiStrapi />,
+  },
+  {
+    tech: "React Router",
+    techLogo: <SiReactrouter />,
+  },
+  {
+    tech: "Context API",
+    techLogo: <AiFillApi />,
+  },
+  {
+    tech: "NextJs",
+    techLogo: <TbBrandNextjs />,
+  },
+  {
+    tech: "Jira",
+    techLogo: <SiJira />,
+  },
+  {
+    tech: "Typescript",
+    techLogo: <SiTypescript />,
+  },
+  {
+    tech: "Material UI",
+    techLogo: <DiMaterializecss />,
+  },
+  {
+    tech: "Tailwind CSS",
+    techLogo: <SiTailwindcss />,
+  },
+  {
+    tech: "Github",
+    techLogo: <BsGithub />,
+  },
+  {
+    tech: "HTML5",
+    techLogo: <GrHtml5 />,
+  },
+  {
+    tech: "CSS3",
+    techLogo: <TbBrandCss3 />,
+  },
+  {
+    tech: "Jest",
+    techLogo: <SiJest />,
+  },
+  {
+    tech: "Web Hosting",
+    techLogo: <SiNetlify />,
+  },
+  {
+    tech: "Javascript",
+    techLogo: <SiJavascript />,
+  },
+  {
+    tech: "AndroidStudio",
+    techLogo: <SiAndroidstudio />,
+  },
+  {
+    tech: "XCode",
+    techLogo: <SiXcode />,
+  },
 ];
